5dbc5a7549fe95a68d8734a6fb5a2232000af7f8
Robert Elek <r@r2.io>
2020-05-23 20:11:59 +0700

    small hls cookie auth fixes

--- html/php/grant_access.php
+++ html/php/grant_access.php
@@ -3,5 +3,12 @@
 // this is just a placeholder for testing, it should do some checking for authentication before
 // used for encrypted HLS playback, to protect the encryption key
 
+// input parameters:
+// X-Original-URI header contains the original uri
+// X-Real-IP header containes users ip address
+
+$uri = $_SERVER['HTTP_X_ORIGINAL_URI'];
+$ip = $_SERVER['HTTP_X_REAL_IP'];
+
 setcookie('video-1-auth', '1', time()+60);
 ?>
